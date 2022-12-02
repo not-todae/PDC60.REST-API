1a590c6fdd185efbd53a626e8945e89d2c6aea00
not-todae <echon.davidaaron@auf.edu.ph>
2022-12-02 12:24:27 +0800

    Add project files.

---
+++ PDC60.REST-API/PDC60.REST-API/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Net.Http;
+
+namespace PDC60.REST_API
+{
+    public class Post
+    {
+
+        public int ID { get; set; }
+        public string username { get; set; }
+
+        public string userpassword { get; set; }
+        public string email { get; set; }
+
+    }
+
+        public partial class MainPage : ContentPage
+        {
+
+            private const string url = "";
+                private HttpClient _Client = new HttpClient();
+            private ObservableCollection<Post> _post;
+            public MainPage()
+            {
+
+
+                InitializeComponent();
+            }
+
+            protected override async void OnAppearing()
+            {
+
+            var content = await _Client.GetStringAsync(url); 
+            var post = JsonConvert.DeserializeObject<List<Post>>(content);
+            _post = new ObservableCollection<Post>(post);
+            Post_List.ItemsSource =_post;
+            base.OnAppearing();
+
+            }
+
+        }
+    }
+
+
